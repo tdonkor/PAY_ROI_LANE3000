94341a74a9699f86b4723dc51cbfb835fc7b56f8
thomasdonkoracrelec <thomas.donkor@acrelec.co.uk>
2021-02-26 10:02:12 +0000

    Add project files.

---
+++ Driver/Pay_ROI_LANE3000/C3NetCommunicator/ConnectionAcceptedEventArgs.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace PAY_ROI_LANE3000.C3NetCommunicator
+{
+    public class  ConnectionAcceptedEventArgs : EventArgs
+    {
+        public bool IsConnected => isConnected;
+        private bool isConnected;
+
+        public ConnectionAcceptedEventArgs(bool isConnected)
+        {
+            this.isConnected = isConnected;
+        }
+    }
+}
---
+++ Driver/Pay_ROI_LANE3000/Program.cs
@@ -0,0 +1,28 @@
+﻿using Acrelec.Library.Logger;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using PAY_ROI_LANE3000.Communicator;
+
+namespace PAY_ROI_LANE3000
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CoreCommunicator coreCommunicator = new CoreCommunicator();
+
+            PayIngenicoLANE3000 payIngenicoLANE3000 = new PayIngenicoLANE3000(coreCommunicator);
+
+            AppDomain.CurrentDomain.UnhandledException += new UnhandledExceptionEventHandler(CurrentDomain_UnhandledException);
+
+            Log.Info(PayIngenicoLANE3000.PAY_INGENICO_LANE3000_LOG, "Pay Conexflow started.");
+        }
+
+    static void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+    {
+        Log.Info(PayIngenicoLANE3000.PAY_INGENICO_LANE3000_LOG, "Unhandled Exception: " + (e.ExceptionObject as Exception).Message);
+    }
+}
+}
